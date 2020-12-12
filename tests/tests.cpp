201d67da6ef9d7d91f6323e019128640502d7990
Marvin Reza <marvinreza@outlook.com>
2020-12-13 00:03:15 +0100

    Add tests for running ECS

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -67,6 +67,78 @@ TEST_CASE("Get system", "[registry|system]")
     Registry registry;
 
     REQUIRE_THROWS_AS(registry.GetSystem<TestSystem>(), std::runtime_error);
-    registry.RegisterSystem<TestSystem>();
+    REQUIRE_NOTHROW(registry.RegisterSystem<TestSystem>());
     REQUIRE_NOTHROW(registry.GetSystem<TestSystem>());
+}
+
+TEST_CASE("Create entity", "[registry|entity]")
+{
+    using namespace ecs;
+    Registry registry;
+
+    REQUIRE_NOTHROW(registry.RegisterComponent<TestData>());
+    REQUIRE_NOTHROW(registry.RegisterComponent<TestData1>());
+    REQUIRE_NOTHROW(registry.CreateEntity().AddComponent<TestData>().AddComponent<TestData1>().Build());
+}
+
+TEST_CASE("Create same entities", "[registry|entity]")
+{
+    using namespace ecs;
+    Registry registry;
+
+    REQUIRE_NOTHROW(registry.RegisterComponent<TestData>());
+    REQUIRE_THROWS_AS(registry.CreateEntity().AddComponent<TestData>().AddComponent<TestData>().Build(), std::runtime_error);
+}
+
+TEST_CASE("Destroy entity", "[registry|entity]")
+{
+    using namespace ecs;
+    Registry registry;
+
+    REQUIRE_NOTHROW(registry.RegisterComponent<TestData>());
+    auto entity = registry.CreateEntity().AddComponent<TestData>().Build();
+    REQUIRE_NOTHROW(registry.HasComponent<TestData>(entity));
+
+    REQUIRE_NOTHROW(registry.GetComponent<TestData>(entity));
+    REQUIRE_NOTHROW(registry.DestroyEntity(entity));
+    REQUIRE_THROWS_AS(registry.GetComponent<TestData>(entity), std::runtime_error);
+}
+
+TEST_CASE("Counting system", "[registry|system]")
+{
+    using namespace ecs;
+    Registry registry;
+
+    REQUIRE_NOTHROW(registry.RegisterComponent<TestData>());
+    REQUIRE_NOTHROW(registry.RegisterComponent<TestData1>());
+
+    class CounterSystem : public System {
+    private:
+        int& m_count0, &m_count1, &m_count2;
+    public:
+        CounterSystem(int& count0, int& count1, int& count2) : m_count0(count0), m_count1(count1), m_count2(count2) { }
+
+        void Run(ComponentAccess& access, EntityQuery& entity_query, tf::Subflow& subflow) override
+        {
+            const auto& td  = access.Read<TestData>();
+            const auto& td1 = access.Read<TestData1>();
+            m_count0 = entity_query().Filter([&td] (Entity e) { return td.HasComponent(e);  }).Entities().size();
+            m_count1 = entity_query().Filter([&td1](Entity e) { return td1.HasComponent(e); }).Entities().size();
+            m_count2 = entity_query().Filter([&td, &td1](Entity e) { return td.HasComponent(e) && td1.HasComponent(e); }).Entities().size();
+        }
+    };
+
+    constexpr int EXPECTED_NUM_ENTITIES = 1024;
+    for (int i = 0; i < EXPECTED_NUM_ENTITIES; ++i) {
+        if (i & 1) REQUIRE_NOTHROW(registry.CreateEntity().AddComponent<TestData>().Build());
+        else REQUIRE_NOTHROW(registry.CreateEntity().AddComponent<TestData1>().Build());
+    }
+
+    int count0 = -1, count1 = -1, count2 = -1;
+    REQUIRE_NOTHROW(registry.RegisterSystem<CounterSystem>(count0, count1, count2));
+
+    REQUIRE_NOTHROW(registry.Run());
+    REQUIRE(count0 == EXPECTED_NUM_ENTITIES / 2);
+    REQUIRE(count1 == EXPECTED_NUM_ENTITIES / 2);
+    REQUIRE(count2 == 0);
 }
