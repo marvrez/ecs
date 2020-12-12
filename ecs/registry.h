535fec0dc8b7bc7cfc131ec7e34f7e937884f338
Marvin Reza <marvinreza@outlook.com>
2020-12-12 21:15:10 +0100

    System methods for Registry
    * Implement methods for registering, retrieving and setting execution
      forders for systems.
    * Add corresponding tests for retrieving and registering systems.

--- ecs/registry.h
+++ ecs/registry.h
@@ -33,10 +33,18 @@ public:
     template <typename ComponentT>
     ComponentT& AddComponent(Entity entity);
 
-    // Get a reference to a component for an entity. A type should be registered in the World and
+    // Get a reference to a component for an entity. A type should be registered in the Registry and
     // an entity should have ComponentT component, otherwise std::runtime_error is thrown.
     template <typename ComponentT>
     ComponentT& GetComponent(Entity entity) { return GetComponentStorage<ComponentT>().GetComponent(entity); }
+
+    // Get a reference to a component given its index
+    // A type should be registered in the Registry and otherwise std::runtime_error is thrown.
+    // index argument should be < than GetNumComponents<ComponentT>()
+    template <typename ComponentT>
+    ComponentT& GetComponentByIndex(ComponentIndex idx) { return GetComponentStorage<ComponentT>()[idx]; }
+    template <typename ComponentT>
+    const ComponentT& GetComponentByIndex(ComponentIndex idx) const { return GetComponentStorage<ComponentT>()[idx]; }
 
     // Get total number of components of a given ComponentT.
     // A type should be registered in the Registry and otherwise std::runtime_error is being thrown.
@@ -60,6 +68,12 @@ public:
     // Get a reference to a system.
     template <typename SystemT>
     SystemT& GetSystem();
+
+    // Make one system SystemT0 run before another system SystemT1.
+    // By default, systems can execute in arbitrary orders -- or even in parallel!
+    // Precede() sets an order of the system execution.
+    template <typename SystemT0, typename SystemT1>
+    void Precede();
 
 private:
     // Get reference to a component storage of a specified type.
@@ -116,6 +130,42 @@ inline ComponentT& Registry::AddComponent(Entity entity)
 {
     std::lock_guard<std::mutex> lock(m_component_mtx);
     return GetComponentStorage<ComponentT>().AddComponent(entity);
+}
+
+template <typename SystemT, typename... Args>
+inline void Registry::RegisterSystem(Args&&... args)
+{
+    std::lock_guard<std::mutex> lock(m_system_mtx);
+    const auto tidx = TypeIndex<SystemT>();
+    if (m_systems.find(tidx) != m_systems.cend()) throw std::runtime_error("Registry: system type already registered");
+
+    SystemInvocation invoke;
+    invoke.system = std::make_unique<SystemT>(std::forward<Args>(args)...);
+    invoke.task   = m_taskflow.emplace([system=invoke.system.get(), this](tf::Subflow& subflow) {
+        ComponentAccess access(*this);
+        EntityQuery     query(*this);
+        system->Run(access, query, subflow);
+    });
+
+    m_systems.emplace(tidx, std::move(invoke));
+}
+
+template <typename SystemT>
+inline SystemT& Registry::GetSystem()
+{
+    const auto tidx = TypeIndex<SystemT>();
+    auto system = m_systems.find(tidx);
+    if (system == m_systems.cend()) throw std::runtime_error("Registry: the specified system type was not found");
+    return reinterpret_cast<SystemT&>(*system->second.system.get());
+}
+
+template <typename SystemT0, typename SystemT1>
+inline void Registry::Precede()
+{
+    const auto tidx0 = TypeIndex<SystemT0>(), tidx1 = TypeIndex<SystemT1>();
+    auto s0 = m_systems.find(tidx0), s1 = m_systems.find(tidx1);
+    if (s0 == m_systems.cend() || s1 == m_systems.cend()) throw std::runtime_error("Registry: one of the specified system types were not found");
+    s0->second.task.precede(s1->second.task);
 }
 
 } // namespace ecs
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,6 +9,10 @@ struct TestData  { float x; };
 struct TestData1 { float x, y; };
 struct TestData2 { float x, y, z; };
 
+struct TestSystem : public ecs::System {
+    virtual void Run(ecs::ComponentAccess& access, ecs::EntityQuery& entity_query, tf::Subflow& subflow) override {}
+};
+
 TEST_CASE("Create registry", "[registry|component]")
 {
     using namespace ecs;
@@ -39,4 +43,30 @@ TEST_CASE("Register multiple component types", "[registry|component]")
     REQUIRE_NOTHROW(registry.RegisterComponent<TestData>());
     REQUIRE_NOTHROW(registry.RegisterComponent<TestData1>());
     REQUIRE_NOTHROW(registry.RegisterComponent<TestData2>());
+}
+
+TEST_CASE("Register system", "[registry|system]")
+{
+    using namespace ecs;
+    Registry registry;
+    REQUIRE_NOTHROW(registry.RegisterSystem<TestSystem>());
+}
+
+TEST_CASE("Register same system", "[registry|system]")
+{
+    using namespace ecs;
+    Registry registry;
+
+    REQUIRE_NOTHROW(registry.RegisterSystem<TestSystem>());
+    REQUIRE_THROWS_AS(registry.RegisterSystem<TestSystem>(), std::runtime_error);
+}
+
+TEST_CASE("Get system", "[registry|system]")
+{
+    using namespace ecs;
+    Registry registry;
+
+    REQUIRE_THROWS_AS(registry.GetSystem<TestSystem>(), std::runtime_error);
+    registry.RegisterSystem<TestSystem>();
+    REQUIRE_NOTHROW(registry.GetSystem<TestSystem>());
 }
